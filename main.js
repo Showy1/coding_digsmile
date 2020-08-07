6af5d990cc6ebb4c5df6c36737a29bc216538ee6
Showy <>
2020-08-07 16:16:44 +0900

    initial

---
+++ main.js
@@ -0,0 +1,30 @@
+$(function(){
+  //include
+  $('#header').load('include/header.html')
+  $('#footer').load('include/footer.html')
+
+  //header ->
+  //hamburger menu
+  $(document).on('click', '#hamburger', function(){
+    $('#nav-mask').fadeToggle(500);
+    $('#hamburger-icon').toggleClass('show');
+  });
+
+  //change header color
+  $(window).on('load scroll', function(){
+    if($(window).scrollTop() < $('#mv-img').height() - 50 ){
+      $('#logo').removeClass('blend');
+    }else{
+      $('#logo').addClass('blend');
+    }
+  });
+  //-> header
+
+  //->main
+  // set mv height
+  var $mv = $('#mv-img')
+  var img = new Image();
+  img.src = $mv.attr('src')
+  $mv.height(img.height);
+  $('.mv, #rect').height(img.height * 1.16);
+});
